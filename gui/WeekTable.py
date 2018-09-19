42d362440bfad36986382aceee01f92a24cff884
shacoli <1142235594@qq.com>
2018-09-19 21:32:10 +0800

    更新了gui界面 正在努力思考优化设计

--- gui/WeekTable.py
+++ gui/WeekTable.py
@@ -1,6 +1,7 @@
 import sys
 import time
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QDate
 from Ui_login import *
 from Ui_WeekTable import *
 sys.path.append("..")
@@ -10,6 +11,9 @@ class WeekTable(QMainWindow,  Ui_WeekTable):
     def __init__(self,  parent=None):
         super(WeekTable,  self).__init__(parent)
         self.setupUi(self)
+        
+        self.dateEdit.setDate(QDate.currentDate())
+        
         self.broadcast_label.setText("")
         self.switch_button.clicked.connect(self.get_day_table)
         self.rescrap_button.clicked.connect(self.get_table_again)
@@ -18,9 +22,9 @@ class WeekTable(QMainWindow,  Ui_WeekTable):
         self.year = self.dateEdit.date().year()
     
     def get_day_table(self):
+        #self.scrollAreaWidgetContents.setMinimumHeight(1800)
         pass
-        
-	
+    
     def get_table_again(self):
         self.broadcast_label.setText("重新抓取用时为 %1.5f" %float(get_table.write_in_my_table(2017030102012, "402561")))
         self.get_day_table()
