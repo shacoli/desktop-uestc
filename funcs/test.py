4f351149fc034304c39006684728ea04866b17dc
shacoli <1142235594@qq.com>
2018-09-19 20:34:22 +0800

    进一步完善了gui界面和逻辑交互

---
+++ funcs/test.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("..")
+from funcs import get_table
+
+print(get_table.write_in_my_table(2017030102012, "402561"))
--- gui/WeekTable.py
+++ gui/WeekTable.py
@@ -1,26 +1,30 @@
 import sys
-import config
+import time
 from PyQt5.QtWidgets import *
 from Ui_login import *
 from Ui_WeekTable import *
 sys.path.append("..")
-from funcs import *
+from funcs import get_table
 
 class WeekTable(QMainWindow,  Ui_WeekTable):
     def __init__(self,  parent=None):
         super(WeekTable,  self).__init__(parent)
         self.setupUi(self)
-        
+        self.broadcast_label.setText("")
         self.switch_button.clicked.connect(self.get_day_table)
-
+        self.rescrap_button.clicked.connect(self.get_table_again)
+        self.day = self.dateEdit.date().day()
+        self.month = self.dateEdit.date().month()
+        self.year = self.dateEdit.date().year()
+    
     def get_day_table(self):
-        day = self.dateEdit.date().day()
-        month = self.dateEdit.date().month()
-        year = self.dateEdit.date().year()
+        pass
+        
 	
     def get_table_again(self):
-        get_table()
-        get_day_table()
+        self.broadcast_label.setText("重新抓取用时为 %1.5f" %float(get_table.write_in_my_table(2017030102012, "402561")))
+        self.get_day_table()
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
